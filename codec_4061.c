c4f188b012008660a4d37d7be57da85c6e0a7930
zachvoll <rippsta@gmail.com>
2017-04-21 00:05:25 -0500

    Update codec_4061.c

--- codec_4061.c
+++ codec_4061.c
@@ -1,14 +1,7 @@
 /*
  * name: Zachary Vollen, Yadu Kiran
- * x500: voll0139, kiran013
- * CSELabs machine: CSEL-KH1200-14
+ * Date: 10/28/2015
  */
- 
- /*
-  * Login: voll0139, kiran013
-  * Date: 10/28/2015
-  * ID: voll0139(4381309), kiran(5183492)
-  */
 
 #include <stdio.h>
 #include <stdlib.h>
